49fc6003afd90652d5634de759d7999ceb0e2f1e
LethaboKgasi <lethabomonyuku@gmail.com>
2026-05-10 21:32:46 +0200

    feat: added live weather conditions using an API.

--- src/index.js
+++ src/index.js
@@ -2,8 +2,17 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#current-temperature");
   let temperature = Math.round(response.data.temperature.current);
   let cityElement = document.querySelector("#current-city");
+  let descriptionElement = document.querySelector("#description");
+  let humidityElement = document.querySelector("#humidity");
+  let windSpeedElement = document.querySelector("#wind-speed");
+
+
+
   cityElement.innerHTML = response.data.city;
-  temperatureElement.innerHTML = temperature;
+  descriptionElement.innerHTML = response.data.condition.description;
+  humidityElement.innerHTML = `${response.data.temperature.humidity}%`;
+  windSpeedElement.innerHTML = `${response.data.wind.speed}km/h`;
+  temperatureElement.innerHTML = Math.round(temperature);
 }
 
 function search(event) {
